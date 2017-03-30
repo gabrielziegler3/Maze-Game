7fa93edacbdfa6c4773335c6cdd90ae7f8a98f8d
Gabriel Ziegler <gabrielziegler3@gmail.com>
2017-03-30 15:37:13 -0300

    Adding Trap class

---
+++ inc/trap.hpp
@@ -0,0 +1,14 @@
+#ifndef TRAP_HPP
+#define TRAP_HPP
+#include "gameobject.hpp"
+#include <iostream>
+
+class Trap : public GameObject {
+
+
+public:
+    Trap();
+    ~Trap();
+    
+};
+#endif
---
+++ src/trap.cpp
@@ -0,0 +1,10 @@
+#include "../inc/trap.hpp"
+#include <iostream>
+
+using namespace std;
+
+Trap::Trap(){
+
+}
+
+Trap::~Trap(){}
