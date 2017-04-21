163122d759a83819838493ebe671993f6b017d5e
Gabriel Ziegler <gabrielziegler3@gmail.com>
2017-04-21 12:56:44 -0300

    Added functions to draw Traps and Bonuses in the map

--- inc/draw.hpp
+++ inc/draw.hpp
@@ -12,14 +12,8 @@ class Draw {
 private:
 public:
     Draw();
-    // void drawPlayer(char **matrix, char sprite, int posx, int posy);
-    // void drawPlayer(char sprite, int posx, int posy);
-    // void drawPlayer2(char **matrix);
-    // void drawPlayer(char matrix[20][50]);
-    // void drawPlayer(Player * player, char **range);
-    void drawTrap();
-    void drawBonus();
+    void drawTrap(Trap * trap, Map * map);
+    void drawBonus(Bonus * bonus, Map *map);
     void drawMap(char **matrix);
-    // void drawMap(char matrix[20][50]);
 };
 #endif
--- src/draw.cpp
+++ src/draw.cpp
@@ -10,12 +10,22 @@
 
 Draw::Draw(){}
 
-void Draw::drawTrap(){
+void Draw::drawTrap(Trap * trap, Map *map){
+    for(int i=0; i<10; i++){
+        trap[i].setPositionX(rand()%18+1);
+        trap[i].setPositionY(rand()%48+1);
+        map->addElement(trap[i].getPositionX(), trap[i].getPositionY(), trap[i].getSprite());
+    }
 }
 
-void Draw::drawBonus(){
-
+void Draw::drawBonus(Bonus * bonus, Map *map){
+    for(int i=0; i<10;i++){
+        bonus[i].setPositionX(rand()%18+1);
+        bonus[i].setPositionY(rand()%48+1);
+        map->addElement(bonus[i].getPositionX(), bonus[i].getPositionY(), bonus[i].getSprite());
+    }
 }
+
 void Draw::drawMap(char **matrix){
     for(int i=0; i < 20; ++i){
         for(int j=0; j< 50; ++j){
@@ -26,35 +36,3 @@ void Draw::drawMap(char **matrix){
         }
     }
 }
-// void Draw::drawMap(char matrix[ROW][COLUMN]){
-//     for(int i=0; i < ROW; i++){
-//         for(int j=0; j< COLUMN; j++){
-//              printw("%c", matrix[i][j]);
-//              if(j == COLUMN -1){
-//                  printw("\n");
-//              }
-//         }
-//     }
-// }
-
-// void Draw::drawPlayer(char **matrix, char sprite, int posx, int posy){
-//     matrix[posx][posy] = sprite;
-// }
-//
-// void Draw::drawPlayer(char sprite, int posx, int posy){
-//     map->addElement(1,1,'@');
-// }
-// void Draw::drawMap(){
-//     int i=0, j=0;
-//     char mapa[i][j];
-// 	for(int i = 0; i < ROW; i++){
-// 		for(int j = 0; j < 50; j++){
-//             stage1 >> mapa[i][j];
-// 	 		printw("%c", this->mapa[i][j]);
-// 	 		if(j >= 49){
-// 	 			printw("\n");
-// 	 		}
-// 		}
-// 	}
-//     getch();
-// }
