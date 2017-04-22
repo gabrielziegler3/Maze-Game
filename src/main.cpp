580397d3899028d84080365424a83c4a4db05c7b
Gabriel Ziegler <gabrielziegler3@gmail.com>
2017-04-22 18:05:45 -0300

    First version of the game fully implemented but missing ranking list

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #include "../inc/player.hpp"
 #include "../inc/trap.hpp"
 #include "../inc/menu.hpp"
+#include "../inc/rankinglist.hpp"
 #include <iostream>
 #include <string>
 #include <ncurses.h>
@@ -13,28 +14,34 @@
 #include <cstdlib>
 
 int main(int argc, char const *argv[]) {
+    bool initialize = false;
+    int counter = 0;
     //Sao instanciados objetos de todas classes
     Draw * draw = new Draw();
     Menu * menu = new Menu();
     Map * map = new Map();
-    Player * player = new Player('@', TRUE, FALSE, 3, 0, 2, 2);
+    Player * player = new Player('@', TRUE, FALSE, 3, 0, 2, 2, "Ziegler");
     Trap * trap = new Trap[10];
     Bonus * bonus = new Bonus[10];
     Collisions * collision = new Collisions();
+    RankingList * ranking = new RankingList();
 
     char ch=menu->mainMenu();
     		switch(ch) {
     			case 1:
     				break;
     			case 2:
+                    clear();
+                    cbreak();
+                    ranking->readList();
     				break;
                 case 3:
                     endwin();
-                    clear();
                     system("clear");
-                    break;
+                    player->setAlive(false);
+                    return(0);
                 }
-    while(player->getAlive() == TRUE){
+    while(player->getAlive() == true){
         // iniciacao do game
         clear();
         initscr(); // switch terminal screen to fullscreen curses mode
@@ -45,25 +52,33 @@ int main(int argc, char const *argv[]) {
         keypad(stdscr, TRUE); // enable keypad mode (for arrow and fn keys)
         noecho(); // do not print the user's keystrokes to the screen
         curs_set(0); // make the cursor invisible
+        map->allocRawMatrix();
         attron(COLOR_PAIR(1));
         printw("Score: [%d]\n", player->getScore());
         printw("Life: [%d]\n\n", player->getLife());
         attroff(COLOR_PAIR(1));
         attron(COLOR_PAIR(2));
-        map->allocMatrix();
-        map->addElement(player->getPositionX(), player->getPositionY(), player->getSprite());
+        map->addElement(player->getPositionY(), player->getPositionX(), player->getSprite());
+        if(initialize == false){
+            draw->drawBonusRandom(bonus, map);
+            draw->drawTrapRandom(trap, map);
+            initialize = true;
+        }
         draw->drawBonus(bonus, map);
         draw->drawTrap(trap, map);
-        draw->drawMap(map->getMatrix());
-        player->move();
-        collision->hitBonus(player, player->getPositionX(), player->getPositionY());
-        collision->hitTrap(player, menu, player->getPositionX(), player->getPositionY());
-        // map->addElement(player->getPositionX(), player->getPositionY(), '-');
-        if(player->getLife()==0){
-            menu->gameOver(player);
-            player->setAlive(false);
-        }
+        draw->drawMap(map->getRawMatrix());
+        player->move(map);
+        collision->hitBonus(player, map, player->getPositionY(), player->getPositionX());
+        collision->hitTrap(player, map, menu, player->getPositionY(), player->getPositionX());
+        collision->hitEnd(player, map, menu, ranking, player->getPositionY(), player->getPositionX());
         refresh();
+        // player->setAlive(false);
+        attroff(COLOR_PAIR(2));
+        if(counter == 5){
+            initialize = false;
+            counter = 0;
+        }
+        counter++;
         endwin();
     }
 
